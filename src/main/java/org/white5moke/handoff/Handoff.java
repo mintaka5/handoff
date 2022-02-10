889bcf4e2c8071fa82147146757f80ee66e415d1
white5moke <chris.is.rad@pm.me>
2022-02-10 08:01:37 -0800

    save point

--- src/main/java/org/white5moke/handoff/Handoff.java
+++ src/main/java/org/white5moke/handoff/Handoff.java
@@ -2,12 +2,32 @@ package org.white5moke.handoff;
 
 import org.white5moke.handoff.document.KeyDocument;
 
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Handoff {
     private Path homeDir;
+    private List<Path> fileList;
 
-    public Handoff() {}
+    public Handoff() {
+        fileList = new ArrayList<>();
+        try {
+            populateFileList();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private List<Path> populateFileList() throws IOException {
+        fileList = Files.list(homeDir).sorted(
+                (a, b) -> Long.compare(b.toFile().lastModified(), a.toFile().lastModified())
+        ).toList();
+
+        return fileList;
+    }
 
     public KeyDocument generateKeyDocument() {
         KeyDocument doc = new KeyDocument();
